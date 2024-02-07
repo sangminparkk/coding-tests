e164f3d746c2e940d5e49f24bf0d0def69c9b51a
chandler <parksm0529@gmail.com>
2024-02-07 11:49:10 +0900

    sol: String.toUpperCase()
    - 문제 : 대문자로 바꾸기
    - 결과 : pass
    - link : https://school.programmers.co.kr/learn/courses/30/lessons/181877

---
+++ src/programmers/basic/lv0/P_181877.java
@@ -0,0 +1,9 @@
+package programmers.basic.lv0;
+
+public class P_181877 {
+    public static String solution(String myString) {
+        String answer = myString.toUpperCase();
+        return answer;
+    }
+}
+
