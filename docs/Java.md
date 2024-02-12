0520ea8fa7cd26779a83287b9eef3df5437983a0
chandler <parksm0529@gmail.com>
2024-02-12 14:45:40 +0900

    sol: copyOfRange() 메소드 적용

---
+++ docs/Java.md
@@ -0,0 +1,6 @@
+## JAVA
+코테 문제에 출제되는 자바 기본 개념 정리
+
+### Arrays (java8)
+  * [copyOfRange](https://docs.oracle.com/javase/8/docs/api/java/util/Arrays.html#copyOfRange-boolean:A-int-int-)
+    * 특정 배열의 특정 구간을 복사할 수 있다. **반복문 사용없이 클린코드로 신규 배열 생성**이 가능하다.
---
+++ src/programmers/basic/lv0/P_181889_ad.java
@@ -0,0 +1,10 @@
+package programmers.basic.lv0;
+
+import java.util.Arrays;
+
+public class P_181889_ad {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = Arrays.copyOfRange(num_list, 0, n);
+        return answer;
+    }
+}
