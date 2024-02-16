2e59eefd086f500b0c5820fdb1d79f4c0325acd6
chandler <parksm0529@gmail.com>
2024-02-16 13:59:38 +0900

    sol: string.startsWith()
    string 메소드 중 prefix와 비교하는 메소드 사용하면 간단하게 해결됨

---
+++ src/programmers/basic/lv0/P_181906_ad.java
@@ -0,0 +1,7 @@
+package programmers.basic.lv0;
+
+public class P_181906_ad {
+    public static int solution(String my_string, String is_prefix) {
+        return my_string.startsWith(is_prefix) ? 1 : 0;
+    }
+}
