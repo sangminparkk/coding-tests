9ee9e04ee6f54445c921c55208a34213189f7d60
chandler <parksm0529@gmail.com>
2024-02-16 15:36:08 +0900

    sol: string.repeat()
    - 문제 : 문자열 곱하기
    - 결과 : pass
    - link : https://school.programmers.co.kr/learn/courses/30/lessons/181940

---
+++ src/programmers/basic/lv0/P_181940.java
@@ -0,0 +1,9 @@
+package programmers.basic.lv0;
+
+public class P_181940 {
+    public static void main(String[] args) {
+    }
+        public String solution(String my_string, int k) {
+            return my_string.repeat(k);
+        }
+}
