37fa830e5ed9d4263ddb6d95ea33205813ad49bc
chandler <parksm0529@gmail.com>
2024-02-21 13:23:59 +0900

    refactor : 순수 파워코딩 (동적배열 + for문 배열복사)

--- src/programmers/basic/lv0/P_181927_ref.java
+++ src/programmers/basic/lv0/P_181927_ref.java
@@ -2,11 +2,18 @@ package programmers.basic.lv0;
 
 public class P_181927_ref {
     public static int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length+1]; // 동적배열
+        // 동적 배열
+        int[] answer = new int[num_list.length+1];
+
+        // 배열 복사 > 값 할당
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[i];
+        }
+
+        // 마지막 원소 할당
         int last = num_list[num_list.length - 1];
         int beforeLast = num_list[num_list.length - 2];
-
-        answer[answer.length-1] = (last > beforeLast) ? last - beforeLast : last * 2;
+        answer[num_list.length] = (last > beforeLast) ? last - beforeLast : last * 2;
 
         return answer;
     }
