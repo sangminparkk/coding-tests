524dcde104a9d42fdef5b57654814c8ca8f513bd
chandler <parksm0529@gmail.com>
2024-02-22 20:00:59 +0900

    refactor: sorted.limit (심플하게)

---
+++ src/programmers/basic/lv0/P_181853_again.java
@@ -0,0 +1,14 @@
+package programmers.basic.lv0;
+
+import java.util.Arrays;
+
+public class P_181853_again {
+
+    public int[] solution(int[] num_list) {
+        return Arrays.stream(num_list)
+                .sorted()
+                .limit(5)
+                .toArray();
+    }
+
+}
