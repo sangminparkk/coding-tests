ec65379d5d52ce00c9ffc43f88ba93a078732449
chandler <parksm0529@gmail.com>
2024-02-23 11:46:00 +0900

    refactor : 한줄코드 처리

--- src/programmers/basic/lv0/P_181937.java
+++ src/programmers/basic/lv0/P_181937.java
@@ -2,7 +2,6 @@ package programmers.basic.lv0;
 
 public class P_181937 {
     public static int solution(int num, int n) {
-        int answer = (num % n == 0) ? 1 : 0; // 클린코드 측면 > 삼항 연산자
-        return answer;
+        return (num % n == 0) ? 1 : 0;
     }
 }
