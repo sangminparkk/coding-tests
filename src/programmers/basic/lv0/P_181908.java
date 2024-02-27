412070dedb01a48b0efb37cf3d3d5b01d94dfdc2
chandler <parksm0529@gmail.com>
2024-02-27 10:27:07 +0900

    sol: str.endsWith()
    - 문제 : 접미사인지 확인하기
    - 결과 : pass
    - link : https://school.programmers.co.kr/learn/courses/30/lessons/181908

---
+++ src/programmers/basic/lv0/P_181908.java
@@ -0,0 +1,8 @@
+package programmers.basic.lv0;
+
+public class P_181908 {
+
+    public int solution(String my_string, String is_suffix) {
+        return my_string.endsWith(is_suffix) ? 1 : 0;
+    }
+}
