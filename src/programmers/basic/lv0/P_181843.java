f88cd8173a12a213c748fd89c5b56116073faaf5
chandler <parksm0529@gmail.com>
2024-02-27 10:30:09 +0900

    sol: str.contains()
    - 문제 : 부분 문자열인지 확인하기
    - 결과 : pass
    - link : https://school.programmers.co.kr/learn/courses/30/lessons/181843#

---
+++ src/programmers/basic/lv0/P_181843.java
@@ -0,0 +1,7 @@
+package programmers.basic.lv0;
+
+public class P_181843 {
+    public static int solution(String my_string, String target) {
+        return my_string.contains(target) ? 1 : 0;
+    }
+}
