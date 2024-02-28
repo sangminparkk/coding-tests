c7025866a8f9f0c64704c448edd86946351b2fe7
chandler <parksm0529@gmail.com>
2024-02-28 13:03:30 +0900

    Fail: 자주 틀리는 문제유형 (컴파일에러)
    - 문제 : 배열의 원소 삭제하기
    - 결과 : Fail
    - link : https://school.programmers.co.kr/learn/courses/30/lessons/181844
    
    - 이슈
    1) 배열의 크기가 동적으로 바뀌는 상황
    2) 1로인해 필터링된 데이터를 새로운 배열에 할당하지 못함

--- src/programmers/basic/lv0/P_181844.java
+++ src/programmers/basic/lv0/P_181844.java
@@ -8,7 +8,7 @@ public class P_181844 {
         for (int i = 0; i < delete_list.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 if (delete_list[i] != arr[j]) {
-                    answer += arr[j]; // 배열의 크기가 정해지지 않았을때, 값 할당 불가
+//                    answer += arr[j]; // 배열의 크기가 정해지지 않았을때, 값 할당 불가
                 }
             }
 
