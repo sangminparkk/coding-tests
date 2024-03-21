b3e9f5bf85c204c8d3266f3867d23587edda79cc
chandler <parksm0529@gmail.com>
2024-03-21 17:41:17 +0900

    sorting 가장 마지막 적용 연산복잡도를 낮추기 위함

--- src/programmers/practice/P_12910.java
+++ src/programmers/practice/P_12910.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 public class P_12910 {
     public static void main(String[] args) {
         P_12910 returnDivisor = new P_12910();
-//        System.out.println(Arrays.toString(returnDivisor.solution(new int[]{5, 9, 7, 10}, 5)));
-        System.out.println(Arrays.toString(returnDivisor.solution(new int[]{3,2,6}, 10)));
+        System.out.println(Arrays.toString(returnDivisor.solution(new int[]{5, 9, 7, 10}, 5)));
+//        System.out.println(Arrays.toString(returnDivisor.solution(new int[]{3,2,6}, 10)));
     }
 
     public int[] solution(int[] arr, int divisor) {
-        Arrays.sort(arr);// 처음부터 sort
-
         int count = 0;
         for (int num : arr) {
             if (num % divisor == 0) {
@@ -19,12 +17,10 @@ public class P_12910 {
             }
         }
 
-        // 이렇게 코드로 쉽게 처리할 수 있었네. >> 현재 나한테 부족한점은
         if (count == 0) {
             return new int[]{-1};
         }
 
-        //아니면 정상출력
         int[] answer = new int[count];
 
         int idx = 0;
@@ -33,6 +29,7 @@ public class P_12910 {
                 answer[idx++] = num;
             }
         }
+        Arrays.sort(arr); // sort팅은 최대한 숫자를 적게 만든다음 적용하는게 좋다.
         return answer;
     }
 }
